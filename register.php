525c3244e90d547bbd2a7bbfcd9e34292213ea75
Eli Barr <niteshadow53@gmail.com>
2017-11-30 10:41:33 -0500

    updated schema and created register.php

--- register.php
+++ register.php
@@ -1,19 +1,26 @@
+<?php
+
 // Given a username and password, create an entry for that user
 // in the database
 
-$username = "user1"
-$password_hash = "ABCDEF"
+$username = "user1";
+$password_hash = "ABCDEF";
 
 // SQL stuff...
 
-$sql_user = 'root'
-$sql_password = 'root'
-$db = 'FinalProjectDB'
-$host = 'localhost'
+$sql_user = 'root';
+$sql_password = 'root';
+$db = 'ncaa_web_app';
+// $host = 'localhost';
+$host = '127.0.0.1';
 $port = 0;
 
-$mysqli = new mysqli($host, $sql_user, $sql_password)
+// MAMP uses 8889 instead of default 3306
+$mysqli = new mysqli($host, $sql_user, $sql_password, $db, 8889);
 
 if ($mysqli->connect_errno){
     echo "Failed to connect";
+    echo ($mysqli->error);
 }
+
+?>
