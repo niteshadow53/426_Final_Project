cf6956c429df5e6106de50a76126844314303f13
Eli Barr <niteshadow53@gmail.com>
2017-12-01 09:20:24 -0500

    PHP reorganization
    Creation of Users.php - an ORM layer for the Users table. Register.php
    reimplemented using this ORM layer.

--- php/Users.php
+++ php/Users.php
@@ -6,7 +6,37 @@
 // array(error=>"", success=>"")
 // where error is any error messages, and success is 1 if successful
 function addUser($username, $hash, $salt){
+    $response = array("error" => "");
 
+    // Connect to mysqli database
+    $mysqli = getMysqliObject();
+
+    // Check for and return connection errors
+    if ($mysqli->connect_errno){
+        $response["error"] = "Failed to connect";
+        $response["error"] .= $mysqli->error;
+        return $response;
+    }
+
+    $qry = "INSERT INTO Users (username, passwd, salt) VALUES ";
+    $qry .= "(\"".$username."\", \"".$hash."\", \"".$salt."\")";
+    // echo "\n".$qry;
+    if (!$mysqli->query($qry)){
+        $response["error"] = $mysqli->error;
+        // echo json_encode($response);
+        return $response;
+    }
+
+    // If we get here, success!
+    $response["success"] = 1;
+    // echo json_encode($response);
+    return $response;
 }
+
+function checkIfUserExists($username){
+    $qry = 
+}
+
+
 
 ?>
--- php/register.php
+++ php/register.php
@@ -1,21 +1,12 @@
-<?php require "utils.php";
+<?php
+require "Users.php";
 
 function register(){
     // Given a username and password, create an entry for that user
     // in the database
-    $response = array("error" => "");
     $username = $_POST["username"];
     $password_hash = "ABCDEF";
     $passwd = $_POST["password"];
-
-    $mysqli = getMysqliObject();
-
-    if ($mysqli->connect_errno){
-        $response["error"] = "Failed to connect";
-        $response["error"] .= $mysqli->error;
-        echo json_encode($response);
-        return;
-    }
 
     // Generate random salt
     $salt = bin2hex(openssl_random_pseudo_bytes(16));
@@ -26,18 +17,18 @@ function register(){
     // echo "\nsalted and hashed: ".$password_hash;
 
     // Store the username, password hash, and salt
-    $qry = "INSERT INTO Users (username, passwd, salt) VALUES ";
-    $qry .= "(\"".$username."\", \"".$password_hash."\", \"".$salt."\")";
-    // echo "\n".$qry;
-    if (!$mysqli->query($qry)){
-        $response["error"] = $mysqli->error;
-        echo json_encode($response);
-        return;
-    }
+    $result = addUser($username, $password_hash, $salt);
 
-    // If we get here, success!
-    $response["success"] = 1;
-    echo json_encode($response);
+    // $qry = "INSERT INTO Users (username, passwd, salt) VALUES ";
+    // $qry .= "(\"".$username."\", \"".$password_hash."\", \"".$salt."\")";
+    // // echo "\n".$qry;
+    // if (!$mysqli->query($qry)){
+    //     $response["error"] = $mysqli->error;
+    //     echo json_encode($response);
+    //     return;
+    // }
+
+    echo json_encode($result);
 }
 
 register();
